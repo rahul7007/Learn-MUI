2d1c8db447b64d8a4a0cf142337f27a4d8ac6847
Rahul Sarma <rahul.sarma@borgos.in>
2021-09-04 21:04:51 +0530

    lesson 10 added

--- material-ui-tut/src/pages/Notes.js
+++ material-ui-tut/src/pages/Notes.js
@@ -1,25 +1,61 @@
 import React, { useState, useEffect } from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Container } from '@material-ui/core';
 
 export default function Notes() {
 
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: 'ABC',
+      details: 'AABBCC'
+    },
+    {
+      id: 2,
+      title: 'MNO',
+      details: 'MMNNOO'
+    },
+    {
+      id: 3,
+      title: 'XYZ',
+      details: 'XXYYZZ'
+    },
+  ])
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => setNotes(data))
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(res => res.json())
+  //     .then(data => setNotes(data))
+  // }, [])
 
 
   return (
-    <div>
-      {
-        notes.map((val) => {
+    <Container>
+      {/* <Grid container>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper> 1 </Paper>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper> 2 </Paper>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper> 3 </Paper>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper> 4 </Paper>
+        </Grid>
+      </Grid> */}
+
+      <Grid container>
+        {notes.map((val) => {
           return (
-            <p key={val.id}>{val.title}</p>
+            <Grid key={val.id} item md={4} sm={6} xs={12}>
+              <Paper>{val.title}</Paper>
+            </Grid>
           )
-        })
-      }
-    </div>
+        })}
+      </Grid>
+    </Container>
   )
 }
