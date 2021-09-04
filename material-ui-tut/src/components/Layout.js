469309bb9a0eac0cb8ae9cbd956963b639d59224
Rahul Sarma <rahul.sarma@borgos.in>
2021-09-04 23:04:56 +0530

    lesson 14 added

--- material-ui-tut/src/components/Layout.js
+++ material-ui-tut/src/components/Layout.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { makeStyles, Typography } from '@material-ui/core'
 import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { SubjectOutlined, AddCircleOutlined } from '@material-ui/icons';
+import { useHistory, useLocation } from 'react-router';
 
 const drawerWidth = 240
 
@@ -17,12 +23,34 @@ const useStyles = makeStyles({
     },
     root: {
         display: 'flex'
+    },
+    active: {
+        backgroundColor: '#f4f4f4'
     }
 })
 
 const Layout = ({ children }) => {
 
     const classes = useStyles()
+
+    const history = useHistory()
+    const location = useLocation()
+
+    const menuItems = [
+        {
+            id: 1,
+            text: 'My Notes',
+            icon: <SubjectOutlined color="secondary" />,
+            path: '/'
+        },
+        {
+            id: 2,
+            text: 'Create Note',
+            icon: <AddCircleOutlined color="secondary" />,
+            path: '/create'
+        },
+    ]
+
     return (
         <div className={classes.root}>
             {/* appbar */}
@@ -40,6 +68,24 @@ const Layout = ({ children }) => {
                         Ninja Notes
                     </Typography>
                 </div>
+
+                {/* lists / links */}
+
+                <List>
+                    {menuItems.map((val) => {
+                        return (
+                            <ListItem
+                                key={val.id}
+                                button
+                                onClick={() => history.push(val.path)}
+                                className={location.pathname === val.path ? classes.active : null}
+                            >
+                                <ListItemIcon>{val.icon}</ListItemIcon>
+                                <ListItemText primary={val.text} />
+                            </ListItem>
+                        )
+                    })}
+                </List>
             </Drawer>
 
             <div className={classes.page}>
