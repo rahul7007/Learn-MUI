5b200812497f630ff1784660466990657a20f4ff
Rahul Sarma <rahul.sarma@borgos.in>
2021-09-04 10:46:34 +0530

    lesson 6 added

--- material-ui-tut/src/App.js
+++ material-ui-tut/src/App.js
@@ -1,19 +1,44 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import CreateCopy from './pages/Create Copy';
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
+
+const myTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#E5BC3F"
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={myTheme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create-copy">
+            <CreateCopy />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- material-ui-tut/src/pages/Create Copy.js
+++ material-ui-tut/src/pages/Create Copy.js
@@ -4,20 +4,39 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import SendIcon from '@material-ui/icons/Send';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
 
-export default function Create() {
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+});
+
+export default function CreateCopy() {
+
+  const classes = useStyles();
 
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
-        color="primary"
+        color="textSecondary"
         component="h2"
         gutterBottom
       >
         Create a new page
       </Typography>
       <Button
+        className={classes.btn}
         type="submit"
         color="secondary"
         variant="contained"
