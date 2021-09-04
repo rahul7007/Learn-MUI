e95420450b60f812973bac79a47fe28e3d3b05fd
Rahul Sarma <rahul.sarma@borgos.in>
2021-09-04 08:32:14 +0530

    lesson 3 added

--- material-ui-tut/src/pages/Create.js
+++ material-ui-tut/src/pages/Create.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -18,6 +20,8 @@ export default function Create() {
         type="submit"
         color="secondary"
         variant="contained"
+        startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
