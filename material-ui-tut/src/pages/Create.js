98a133d45f67e0b901de2814c862f5d6bc330d1a
Rahul Sarma <rahul.sarma@borgos.in>
2021-09-04 08:45:11 +0530

    lesson 5 added

--- material-ui-tut/src/pages/Create.js
+++ material-ui-tut/src/pages/Create.js
@@ -4,11 +4,30 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import SendIcon from '@material-ui/icons/Send';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+});
 
 export default function Create() {
+
+  const classes = useStyles();
+
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -17,6 +36,7 @@ export default function Create() {
         Create a new page
       </Typography>
       <Button
+        className={classes.btn}
         type="submit"
         color="secondary"
         variant="contained"
